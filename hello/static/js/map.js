b134031bce94d1ee573698faaa03fdb9bba853b6
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2020-07-05 16:43:12 -0500

    addDB

--- hello/static/js/map.js
+++ hello/static/js/map.js
@@ -1,12 +1,9 @@
+window.alert("sometext");
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZW1hbnNodSIsImEiOiJja2MxYm51d2EwZWI4MnB2dGNqNXk1amowIn0._GhY7C1S3tbc_w3ULJq3QQ';
 var map = new mapboxgl.Map({
-container: '<your HTML element id>',
+container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
 center: [-74.5, 40], // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
-
-
-let d = new Date();
-      document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
