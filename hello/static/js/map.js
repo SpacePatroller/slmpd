e462c81720fdd21273f548b6aaf81d540535fd8e
SpacePatroller <emanshupatel@Emanshus-MacBook-Pro.local>
2020-07-11 17:42:58 -0500

    gitinore test

--- hello/static/js/map.js
+++ hello/static/js/map.js
@@ -1,9 +1,10 @@
-window.alert("sometext");
+// window.alert("sometext");
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZW1hbnNodSIsImEiOiJja2MxYm51d2EwZWI4MnB2dGNqNXk1amowIn0._GhY7C1S3tbc_w3ULJq3QQ';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZW1hbnNodSIsImEiOiJja2MxYm51d2EwZWI4MnB2dGNqNXk1amowIn0._GhY7C1S3tbc_w3ULJq3QQ'
 var map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
-});
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+})
